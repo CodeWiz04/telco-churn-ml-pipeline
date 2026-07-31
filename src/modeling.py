b64b21433175bda8fb48c3b883c906c5680c511f
Shafan Ali <muhammadshafan2004@gmail.com>
2026-07-31 15:10:59 +0500

    feat(modeling): add train_model function to fit the model

--- src/modeling.py
+++ src/modeling.py
@@ -12,6 +12,21 @@ def build_baseline_model(
     model=LogisticRegression(
         class_weight=class_weights,
         random_state=42,
-        max_iter=1000,
+        max_iter=1000,   #iterate upto 1000 iterations to find the best weight
     )
     return model
+
+def train_model(
+    model,
+    X_train,
+    y_train,
+):
+    """
+    Train the model.
+    """
+    model.fit(
+        X_train,
+        y_train,
+    )
+    return model
+
